e3708fb979fcc1994dfa818f9ed8093d3135c013
아이리스 <eyelessiris@hotmail.com>
2025-12-22 10:40:31 +0900

    Migrate project to Kotlin DSL and update package
    Replaced Groovy-based Gradle files with Kotlin DSL equivalents, updated project group and root name to 'com.reah' and 'yuja', and migrated main and test application classes from 'com.example.demo' to 'com.reah.yuja'. Updated application properties and dependencies to reflect new structure and requirements.

---
+++ src/main/java/com/reah/yuja/YujaApplication.java
@@ -0,0 +1,13 @@
+package com.reah.yuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YujaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YujaApplication.class, args);
+	}
+
+}
--- src/test/java/com/reah/yuja/YujaApplicationTests.java
+++ src/test/java/com/reah/yuja/YujaApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.reah.yuja;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class YujaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
